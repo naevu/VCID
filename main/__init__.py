b02bf778f13f6595730b7ec71fa5fea5a090cfbc
naevu <98766984+naevu@users.noreply.github.com>
2022-09-08 23:26:11 +0200

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/sc-phoenix.db'
 app.config['SECRET_KEY'] = '46qw87esad435asdqw87e6'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 EVENT_TYPES = ["Training", "Match", "Tournament"]
 USER_ROLES = ["ADMIN", "NORMAL"]
